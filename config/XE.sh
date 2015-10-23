a164d446474ae911c10d39c237a13ae9a87fb139
Gleb Kuzmenko <bayclosed@gmail.com>
2015-10-23 10:53:11 +0200

    improvement to startup sequence
    HOST is to be substituted after the softlinks are re-created,
    not before

--- config/XE.sh
+++ config/XE.sh
@@ -13,7 +13,7 @@ umask ${OLD_UMASK}
 mkdir -p /u01/app/oracle/oradata/XE
 ORACLE_SID=XE; export ORACLE_SID
 
-for f in {diag,admin,fast_recovery_area,oradata,oradiag_oracle} product/11.2.0/xe/{dbs,log,config/log}
+for f in {diag,admin,fast_recovery_area,oradata} product/11.2.0/xe/{dbs,log,config/log,network/admin}
 do mv /u01/app/oracle/$f /oracle/${f//\//_} && ln -s /oracle/${f//\//_} /u01/app/oracle/$f
 done
 
